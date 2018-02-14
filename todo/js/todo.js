730004f9518ccac5b87a528235ccc7a6c95ddff4
Mario Knecht <mario.knecht@gmail.com>
2018-02-14 22:54:07 +0100

    modify to eventdelegation

--- todo/js/todo.js
+++ todo/js/todo.js
@@ -1,36 +1,48 @@
 
+var taskJSON = '[{"todo":"Salat"},{"todo":"Milch"}]';
 
+var taskList;// = JSON.parse(taskJSON);
 
-var list = document.querySelector('.todo__list')
-    .addEventListener('click',function(e){
-        console.log('remove');
-        console.log(e.target);
+if(localStorage.getItem('taskList')){
+    taskList = JSON.parse(localStorage.getItem('taskList'));
+}else{
+    taskList = JSON.parse(taskJSON);
+}
+
+console.log(taskList);
+//showContent();
+function showContent(){
+    taskList.forEach(function(element,index){
+        console.log(element.todo,index);
+    })
+}
+
+var list = document.querySelector('.todo__list');
+console.log(list);
+
+//remove list item
+   list.addEventListener('click',function(e){
         if(e.target && e.target.matches('li .list__removeicon')){
-            console.log('in');
+
+            var removeId = e.target.parentNode.parentNode.parentNode.id;
+            console.log(removeId);
+            taskList.splice(removeId,1);
+            renderList();
+            console.log(e.target.parentNode.parentNode.parentNode.id);
+            //console.log(e.target.parentNode.parentNode.parentNode.parentNode
+                //.removeChild(e.target.parentNode.parentNode.parentNode));
         }
     });
 
-
-document.querySelector('#addtxt')
-    .addEventListener('keydown',addNewItem);
-
-function getInput(){
-    return document.querySelector('#addtxt')
-        .value
-}
-
-function defaultInput(){
-    return document.querySelector('#addtxt')
-        .value = '';
-}
-
-function addNewItem(e){
-
-    if(e.code === 'Enter'){
-        var inputTxt = getInput();
-
+renderList();
+function renderList(){
+    //console.log(list);
+    list.innerHTML = ''; //delete li elements(means the list content)
+    taskList.forEach(function(element,index){
+        console.log(element.todo,index);
         var listItem = document.createElement('li');
         listItem.classList.add('list__item','list__item--show');
+        listItem.id = index;
         //checkbox
         var boxCheck = document.createElement('div');
         boxCheck.classList.add('list__taskdone');
@@ -52,29 +64,45 @@ function addNewItem(e){
         btnDel.classList.add('btn','btnremove');
         var imgDel = document.createElement('img');
         imgDel.classList.add('img_base','list__removeicon');
-        imgDel.setAttribute('src','img/close.svg')
+        imgDel.setAttribute('src','img/close.svg');
 
+        list.appendChild(listItem);
+        listItem.appendChild(boxCheck);
+        boxCheck.appendChild(inputCheck);
+        boxCheck.appendChild(labelCheck);
+        labelCheck.appendChild(labelInner);
+        listItem.appendChild(txtInner).innerHTML = element.todo;
+        listItem.appendChild(boxDel)
+        boxDel.appendChild(btnDel)
+        btnDel.appendChild(imgDel);
+    })
+    saveLocal();
+}
 
-        //create listElement
-        document.querySelector('.todo__list')
-            .appendChild(listItem)
-            listItem.appendChild(boxCheck)
-            boxCheck.appendChild(inputCheck)
-            boxCheck.appendChild(labelCheck)
-            labelCheck.appendChild(labelInner)
-            listItem.appendChild(txtInner).innerText = inputTxt
-            listItem.appendChild(boxDel)
-            boxDel.appendChild(btnDel)
-            btnDel.appendChild(imgDel);
+document.querySelector('#addtxt')
+     .addEventListener('keydown',addNewItem);
 
-        //delete value of inputfield
+function addNewItem(e){
+
+    if(e.code === 'Enter'){
+        var inputTxt = getInput();
+        taskList.push({todo:inputTxt});
         defaultInput();
+        renderList();
     }
+}
+
+function getInput(){
+    return document.querySelector('#addtxt')
+        .value
+}
+
+function defaultInput(){
+    return document.querySelector('#addtxt')
+        .value = '';
+}
 
 
-
-
-
-
-
+function saveLocal(){
+    localStorage.setItem(JSON.stringify(taskList));
 }
