41d015da47c41ff2f519b2856f80b40e09a7f411
Mario Knecht <mario.knecht@gmail.com>
2018-02-15 19:16:16 +0100

    describe uebung_1_1 module pattern

--- javascript/07_Module/lektion7/js/tools.js
+++ javascript/07_Module/lektion7/js/tools.js
@@ -1,5 +1,5 @@
 var Tools = (function(){
-    var removeElement = function(element){
+    var removeElement = function(element){//only give the element which has to be deleted(to use in several cases)
 
         element.parentNode.removeChild(element);
     };
@@ -10,7 +10,9 @@ var Tools = (function(){
             return function check(e){  //on click this function is executed, event is like when a normal click function is executed
                if(e.target && e.target.matches(target)){
                    console.log('callback event: '+e);
-                   callback(e);//the function to remove the element is called e -> event
+                   callback(e);//the function to remove the element is called e -> event(this is the function in the html doc which is called)
+                               //the callback function can also have other names.
+                               //because we call the function and an event is required we have to send the event
                }
             }
 
