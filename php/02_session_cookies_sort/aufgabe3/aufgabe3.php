54d255e3f97febcf14ec5b139fc8f1af5e5fb5fd
Mario Knecht <mario.knecht@gmail.com>
2018-04-30 18:21:18 +0200

    fixed login problem, wrong rule

--- php/02_session_cookies_sort/aufgabe3/aufgabe3.php
+++ php/02_session_cookies_sort/aufgabe3/aufgabe3.php
@@ -8,17 +8,21 @@ if(isset($_GET['logout'])){
     unset($_SESSION["username"]);
 }
 
+//setLoginForm();
+
 //check if $_GET contains username and this one is not empty
 //set usernmae in $_SESSION variable
-if(isset($_GET['username']) && !empty($_GET['username']) && isset($_GET['username'])=='mario') {
+if(isset($_GET['username']) && !empty($_GET['username']) && $_GET['username']=='mario') {
     $username = $_GET['username'];
     //echo $username;
     $_SESSION['username'] = $username;
-    //print_r($_SESSION);
-}elseif(isset($_GET['username']) && !empty($_GET['username']) && isset($_GET['username'])!='mario'&&!isset($_GET['logout'])){
+    print_r($_SESSION);
+}elseif(isset($_GET['username']) && !empty($_GET['username']) && $_GET['username']!='mario'){
+    setLoginForm();
     echo 'Benutzername ungültig!<br>';
     echo 'Try again... ;-)';
-}
+    return;
+ }
 
 if(!empty($_SESSION['username']) && $_SESSION['username']=='mario'){
     $username = $_SESSION['username'];
@@ -27,10 +31,13 @@ if(!empty($_SESSION['username']) && $_SESSION['username']=='mario'){
     require '../../01_repetition/aufgabe2/aufgabe2.php';
 
     echo '<p></p><a href="?logout">logout</a></p>';
+}elseif(isset($_GET['username']) && !empty($_GET['username']) && isset($_GET['username'])!='mario'&&!isset($_GET['logout'])){
+    echo 'Benutzername ungültig!<br>';
+    echo 'Try again... ;-)';
+
 }else{
     setLoginForm();
     echo 'Es ist niemand eingeloggt!';
-
 }
 
 
